5f61650c75e10fe954e740d701aab90590f611fd
busquedad <busqedad@gmail.com>
2019-05-02 17:10:47 -0300

    Actividad completada - banzaiiii .... en proceso de refinado

--- mod2_cmd_iterators.js
+++ mod2_cmd_iterators.js
@@ -17,8 +17,35 @@ printRoutes();
    seguido	 del	 string	 “:	 “	 y	 del	 número	 de	 veces	 que	 esta	 repe<do.	 	 Si	 se	 añade	 la	 opción	 -r
  delante	de	un	parámetro	este	se	elimina	y	no	aparece	en	el	listado. */
 
-//rest.push("uno", "-r", "dos", "uno", "dos", "tres", "tres", "dos", "tres");
+rest.push("uno", "-r", "dos", "uno", "dos", "tres", "tres", "dos", "tres", "-r");
 
+console.log(`${rest}`);
+
+function clearArray(array) {
+    for (let index = 0 ; index <= array.length ; index++){
+        let discard = rest.indexOf('-r');
+        if (discard >= 0){
+            rest.splice(discard,1);
+        }
+    }
+    return rest.sort();
+}
+
+clearArray(rest);
+
+console.log(`${rest}`);
+
+function arrayDictionary(array) {
+    let dictionary = [];
+    dictionary = array.reduce((countElement, element) => {
+        dictionary[element] = (dictionary[element] || 0) + 1;
+        return dictionary;
+    },1);
+    return console.log(`${dictionary}`)
+}
+arrayDictionary(rest);
+
+/*
 let discard = rest.indexOf('-r');
 
 rest.splice(discard,0);
@@ -30,6 +57,17 @@ let Dictionary = [];
 Dictionary = rest.reduce((countElement, element) => {
     Dictionary[element] = (Dictionary[element] || 0) + 1;
     return Dictionary;
-});
+},1);
 
 console.log(Dictionary);
+
+function arrayDictionary(array) {
+    let dictionary = [];
+    dictionary = rest.reduce((countElement, element) => {
+        dictionary[element] = (dictionary[element] || 0) + 1;
+        return dictionary;
+    },1);
+    return console.log(`${dictionary}`)
+}
+arrayDictionary(rest);
+*/
